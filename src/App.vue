93a3e29a56cc013aa36623f150535df0b9f91d78
Henrique Batista <wellington.h.batista@hotmail.com>
2023-08-26 16:12:33 -0300

    Cria logo `LogoWeon`

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,80 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app class="app-font">
     <v-main>
-      <router-view />
+      <div class="top">
+        <div class="topColor"></div>
+      </div>
+      <div class="center-hv">
+        <Logo />
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Logo from "@/components/atoms/LogoWeon.vue";
 export default {
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  components: { Logo },
 };
 </script>
+
+<style>
+.top {
+  display: grid;
+  grid-template-columns: 100%;
+  grid-auto-rows: 1rem;
+}
+.topColor {
+  margin-top: 0;
+  margin-left: 0;
+  background-image: rgb(2, 0, 36);
+  background-image: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 58, 121, 0.8465511204481793) 31%,
+    rgba(0, 212, 255, 1) 100%
+  );
+}
+html {
+  overflow-y: auto;
+}
+body {
+  font-family: "Roboto";
+}
+.center-hv {
+  margin: auto;
+  position: absolute;
+  width: 100;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.app-font {
+  /* font-family: "Gill Sans Nova", sans-serif !important;
+  font-weight: bold !important; */
+}
+
+::-webkit-scrollbar-track {
+  background-color: #ffffff;
+}
+
+::-webkit-scrollbar {
+  width: 15px;
+  background-color: #f5f5f5;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #6e6e6e;
+  border-radius: 10px;
+  border-right: 3px #ffffff solid;
+  border-left: 3px #ffffff solid;
+  border-top: 2px white solid;
+  border-bottom: 2px white solid;
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: #9e9e9e;
+}
+</style>
---
+++ src/components/atoms/LogoWeon.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-img class="logo" src="../../assets/Logo-WeON.png" alt="Logo Weon"></v-img>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="css" scoped>
+.logo {
+  width: 100%;
+}
+</style>
